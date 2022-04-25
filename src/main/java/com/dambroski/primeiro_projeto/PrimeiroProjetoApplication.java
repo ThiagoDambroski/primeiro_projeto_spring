02ae95ba94d12071b9c83465bb4073ce70763cd9
ThiagoDambroski <92185785+ThiagoDambroski@users.noreply.github.com>
2022-04-25 09:08:41 -0300

    iniciando com dados

--- src/main/java/com/dambroski/primeiro_projeto/PrimeiroProjetoApplication.java
+++ src/main/java/com/dambroski/primeiro_projeto/PrimeiroProjetoApplication.java
@@ -1,13 +1,34 @@
 package com.dambroski.primeiro_projeto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dambroski.primeiro_projeto.domain.Categoria;
+import com.dambroski.primeiro_projeto.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PrimeiroProjetoApplication {
+public class PrimeiroProjetoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
